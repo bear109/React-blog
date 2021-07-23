f21dcdce377917f0f7e345cc2c8649671226bf0b
bear109 <jokerbear109@gmail.com>
2021-07-23 13:47:09 +0900

    auth 모듈 수정(리덕스 적용)

--- blog-frontend/src/modules/auth.js
+++ blog-frontend/src/modules/auth.js
@@ -1,8 +1,11 @@
 import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
 
+//액션 생성
 const CHANGE_FIELD = 'auth/CHANGE_FIELD';
 const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
+//액션 함수 정의
 export const changeField = createAction(
   CHANGE_FIELD,
   ({ form, key, value }) => ({
@@ -12,8 +15,10 @@ export const changeField = createAction(
   }),
 );
 
+//초기 상태 폼
 export const initializeForm = createAction(INITIALIZE_FORM, (form) => form); //register / login
 
+//초기 상태 함수
 const initialState = {
   register: {
     username: '',
@@ -26,9 +31,17 @@ const initialState = {
   },
 };
 
+//리듀서(액션 핸들링)
 const auth = handleActions(
   {
-    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) => state,
+    [CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
+      produce(state, (draft) => {
+        draft[form][key] = value;
+      }),
+    [INITIALIZE_FORM]: (state, { payload: form }) => ({
+      ...state,
+      [form]: initialState[form],
+    }),
   },
   initialState,
 );
