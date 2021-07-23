f7a1b7d5599bfce4293e77acb72374a3a3ab9c74
bear109 <jokerbear109@gmail.com>
2021-07-23 17:36:34 +0900

    회원가입 구현1

--- blog-frontend/src/containers/auth/RegisterForm.js
+++ blog-frontend/src/containers/auth/RegisterForm.js
@@ -2,12 +2,15 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import AuthForm from '../../components/auth/AuthForm';
+import { register } from '../../lib/api/auth';
 import { changeField, initializeForm } from '../../modules/auth';
 
 const RegisterForm = () => {
   const dispatch = useDispatch();
-  const { form } = useSelector(({ auth }) => ({
+  const { form, auth, authError } = useSelector(({ auth }) => ({
     form: auth.register,
+    auth: auth.auth,
+    authError: auth.authError,
   }));
 
   // 인풋 변경 이벤트 핸들러
@@ -25,12 +28,30 @@ const RegisterForm = () => {
   // 폼 등록 이벤트 핸들러
   const onSubmit = (e) => {
     e.preventDefault();
+    const { username, password, passwordConfirm } = form;
+    if (password !== passwordConfirm) {
+      return;
+    }
+    dispatch(register({ username, password }));
   };
 
   //초기 렌더링시 폼 초기화
   useEffect(() => {
     dispatch(initializeForm('register'));
   }, [dispatch]);
+
+  //회원가입 성공/실패 처리
+  useEffect(() => {
+    if (authError) {
+      console.log('오류 발생');
+      console.log(authError);
+      return;
+    }
+    if (auth) {
+      console.log('회원가입 성공');
+      console.log(auth);
+    }
+  }, [auth, authError]);
 
   return (
     <AuthForm
