09d2f4706b6c1ab58674c64809ea6b9d58c3acc6
bear109 <jokerbear109@gmail.com>
2021-07-23 20:01:08 +0900

    헤더 컴포넌트 생성

---
+++ blog-frontend/src/components/common/Responsive.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import styled from 'styled-components';
+
+const ResponsiveBlock = styled.div`
+  padding-left: 1rem;
+  padding-right: 1rem;
+  width: 1024px;
+  margin: 0 auto;
+
+  @media (max-width: 1024px) {
+    width: 768px;
+  }
+  @media (max-width: 768px) {
+    width: 100%;
+  }
+`;
+
+const Responsive = ({ children, ...rest }) => {
+  // ...rest를 사용하여 ResponsiveBlock에 이벤트 전달
+  return <ResponsiveBlock {...rest}>{children}</ResponsiveBlock>;
+};
+
+export default Responsive;
