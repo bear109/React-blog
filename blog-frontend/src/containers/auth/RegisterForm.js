5709b4e392b46424b5b5bd0b146696d40d79a3cf
bear109 <jokerbear109@gmail.com>
2021-07-23 18:41:52 +0900

    사용자 상태 관리

--- blog-frontend/src/containers/auth/RegisterForm.js
+++ blog-frontend/src/containers/auth/RegisterForm.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import AuthForm from '../../components/auth/AuthForm';
+import { check } from '../../modules/user';
 import { changeField, initializeForm, register } from '../../modules/auth';
 
 const RegisterForm = () => {
   const dispatch = useDispatch();
-  const { form, auth, authError } = useSelector(({ auth }) => ({
+  const { form, auth, authError, user } = useSelector(({ auth, user }) => ({
     form: auth.register,
     auth: auth.auth,
     authError: auth.authError,
+    user: user.user,
   }));
 
   // 인풋 변경 이벤트 핸들러
@@ -49,8 +51,16 @@ const RegisterForm = () => {
     if (auth) {
       console.log('회원가입 성공');
       console.log(auth);
+      dispatch(check());
     }
-  }, [auth, authError]);
+  }, [auth, authError, dispatch]);
+
+  useEffect(() => {
+    if (user) {
+      console.log('check API 성공');
+      console.log(user);
+    }
+  }, [user]);
 
   return (
     <AuthForm
